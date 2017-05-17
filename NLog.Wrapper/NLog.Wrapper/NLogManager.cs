9f812f0c541a77d31c36e85d1373fa3e775ad377
曹麟 <caol@youlijinfu.com>
2017-05-17 17:49:30 +0800

    fix comment

--- NLog.Wrapper/NLog.Wrapper/NLogManager.cs
+++ NLog.Wrapper/NLog.Wrapper/NLogManager.cs
@@ -1,12 +1,17 @@
-﻿using Common.Logging.Configuration;
+﻿using System.IO;
+using Common.Logging.Configuration;
 using Common.Logging.NLog;
 
 namespace NLog.Wrapper
 {
+    /// <summary>
+    /// NLog 管理类
+    /// </summary>
     public class NLogManager
     {
         public static void Init()
         {
+            // Common.Logging 已内置文件存在判断等前置逻辑
             Init("NLog.config");
         }
 
