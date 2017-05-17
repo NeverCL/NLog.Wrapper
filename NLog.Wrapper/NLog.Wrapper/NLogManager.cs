276caf44a016d7274c053bfb8423f5349f786ca9
曹麟 <caol@youlijinfu.com>
2017-05-17 17:40:07 +0800

    add common.logging

---
+++ NLog.Wrapper/NLog.Wrapper/NLogManager.cs
@@ -0,0 +1,18 @@
+﻿using Common.Logging.Configuration;
+using Common.Logging.NLog;
+
+namespace NLog.Wrapper
+{
+    public class NLogManager
+    {
+        public static void Init()
+        {
+            Init("NLog.config");
+        }
+
+        public static void Init(string configFile)
+        {
+            global::Common.Logging.LogManager.Adapter = new NLogLoggerFactoryAdapter(new NameValueCollection { { "configFile", configFile } });
+        }
+    }
+}
